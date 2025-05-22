c9988856a2f333b795768dcd07c1e6e96ad76243
spoonligma <riley21rivera@gmail.com>
2025-05-22 19:14:54 -0400

    added json and made public dir

---
+++ public/firebase-config.example.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_AUTH_DOMAIN_HERE",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE"
+};
+
+export default firebaseConfig;
